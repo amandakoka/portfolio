2621eef38cb04e9caf4247f55e8af67bee18e03c
🅰️manda <amandakoka@icloud.com>
2026-07-22 21:45:26 +0100

    feat: add initial navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,13 @@
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <header className="border-b">
+      <div className="px-6 py-6">
+        <a href="#home" className="text-4xl font-bold">
+          Amanda Koka
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
